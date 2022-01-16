83e684e17f142cf39d2b6ffab5786496530b025c
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-01-16 22:19:54 +0530

    Create InsertionSort.java
    Added Insertion Sort

---
+++ DSA/src/array/InsertionSort.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+	public void InsertionSorting (int nums[]) {
+		int temp;
+		for ( int i = 1 ; i < nums.length ; i++) {
+			temp = nums[i];
+			int j = i - 1;
+			while (j >= 0 && temp < nums[j] ) {
+				nums[j+1] = nums[j];
+			}
+			nums[j+1] = temp;
+		}
+		
+		for ( int i = 0 ; i < nums.length ; i++ ) {
+			System.out.println(nums[i]); 
+		}
+	}
+	
+	public static void main(String args[]) {
+		Scanner scObj = new Scanner(System.in);
+		int n = scObj.nextInt();
+		int nums[] = new int[n];
+		for ( int i = 0 ; i < n ; i++ ) {
+			nums[i] = scObj.nextInt();
+		}
+		InsertionSort newObj = new InsertionSort();
+		newObj.InsertionSorting(nums);
+	}
+}
