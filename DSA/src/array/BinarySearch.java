7057ff12843646e971947e75646935caa70a5e3b
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-01-20 00:37:54 +0530

    Update BinarySearch.java

--- DSA/src/array/BinarySearch.java
+++ DSA/src/array/BinarySearch.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BinarySearch {
 
 	public int findElement (int nums[] , int elementToSearch ) {
-		int high = nums.length;
+		int high = nums.length - 1;
 		int low = 0;
 		while (low <= high) {
 			int mid = (low+high)/2;
@@ -22,6 +22,23 @@ public class BinarySearch {
 		return -1;
 	}
 	
+	public int findElementRecursion(int nums[], int elementToSearch, int low , int high) {
+		if(low <= high) {
+			int mid = (low + high)/2;
+			if(nums[mid] == elementToSearch ) {
+				return mid;
+			}
+			
+			if( nums[mid] > elementToSearch ) {
+				return findElementRecursion(nums,elementToSearch,low,mid-1);
+			}else {
+				return findElementRecursion(nums,elementToSearch,mid+1,high);
+			}
+			
+		}
+		return -1;
+	}
+	
 	public static void main(String arg[]) {
 		Scanner ScObj = new Scanner(System.in);
 		int n = ScObj.nextInt();
@@ -32,6 +49,7 @@ public class BinarySearch {
 		
 		int  elementToSearch = ScObj.nextInt();
 		BinarySearch bsObj = new BinarySearch();
-		System.out.println(bsObj.findElement(a ,elementToSearch));
+//		System.out.println(bsObj.findElement(a ,elementToSearch));
+		System.out.println(bsObj.findElementRecursion(a,elementToSearch,0,a.length-1));
 	}
 }
