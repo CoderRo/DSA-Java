d0587892e0d4704cfa3e381f883371d8b136c5ae
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-02-02 23:45:32 +0530

    Create TrailingZerosCount.java

---
+++ DSA/src/bitManipulation/TrailingZerosCount.java
@@ -0,0 +1,26 @@
+package bitManipulation;
+import java.util.Scanner;
+
+public class TrailingZerosCount {
+    public int solve(int A) {
+        int count = 0;
+        while(A!=0){
+            if( (A & 1) == 0) {
+                count++;
+            }else{
+                break;
+            }
+            A = A>>1;
+        }
+        return count;
+    }
+    public static void main(String args[]) {
+    	Scanner scObj = new Scanner(System.in);
+    	int n = scObj.nextInt();
+    	TrailingZerosCount newObj = new TrailingZerosCount();
+    	System.out.println(Integer.toBinaryString(n));
+    	System.out.println(newObj.solve(n));
+    	
+    }
+
+}
