37524aae0035722a13706bf774308b91043328ba
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-02-03 23:13:02 +0530

    Update DivideNumber.java

--- DSA/src/Math/DivideNumber.java
+++ DSA/src/Math/DivideNumber.java
@@ -8,10 +8,26 @@ public class DivideNumber {
 		int a = scObj.nextInt();
 		int b = scObj.nextInt();
 		DivideNumber newObj = new DivideNumber();
-		System.out.println(newObj.divideNum(a,b));
+		System.out.println(newObj.divideNum1(a,b));
 	}
 
-	//Brute-force
+	//Log(n)
+	public int divideNum1(int a, int b) {
+		int sign = ((a>0)^(b>0)) ? -1 : 1;
+		int quotient = 0;
+		while(a >= b) {
+			int divisor = b; int itr = 0;
+			while(a >= divisor) {
+				itr++;
+				divisor=b*(1<<itr);
+			}
+			quotient+=1<<(itr-1);
+			a-=b*(1<<(itr-1));
+		}
+		return quotient*sign ;
+	}
+	
+    //Brute-force timeComplexity ->O(n) 
 	public int divideNum(int a, int b) {
 		int sign = ((a>0) ^ (b>0)) ? -1 : 1;
 		int quotient = 0;
