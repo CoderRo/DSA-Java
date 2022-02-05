1e7be4ee10208fced032dcea44cb4d271277ce97
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-02-05 18:42:07 +0530

    Create abc.java

---
+++ DSA/src/bitManipulation/abc.java
@@ -0,0 +1,35 @@
+package bitManipulation;
+
+import java.util.Scanner;
+
+public class abc {
+	
+    public int cntBits(int[] A) {
+        int n = A.length;
+        int int_size = 32;
+        long count = 0;
+        long totalCount = 0;
+        for( int j = 0 ; j < int_size ; j++) {
+        	count = 0;
+            for ( int i = 0 ; i < n ; i++ ) {
+                if((A[i] & (1<<j)) != 0) {
+                	count++;
+                }
+                totalCount += 2*(n - count)*count;  
+                totalCount %=(Math.pow(10,9)+7);
+            }
+        }
+        return (int) (totalCount%(Math.pow(10,9)+7));
+    }
+    
+	public static void main(String args[]) {
+		Scanner ScObj = new Scanner(System.in);
+		int n = ScObj.nextInt();
+		int num[] = new int[n]; 
+		for( int i = 0 ; i < n ; i++) {
+			num[i] = ScObj.nextInt();
+		}
+		abc newObj = new abc();
+		newObj.cntBits(num);
+	}
+}
