e068b1cb8ee0e0a74f0f670c30c6ace5048227bb
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-05-31 00:41:12 +0530

    Update reverseString2.java

--- DSA/src/string/reverseString2.java
+++ DSA/src/string/reverseString2.java
@@ -10,10 +10,7 @@ public class reverseString2 {
 		return str;
 	}
 
-	public static char[] reverseString(char[] str) {
-		int strLen = str.length;
-		int s = 0;
-		int e = strLen - 1;
+	public static char[] reverseString(char[] str, int s ,int e) {
 		while(s < e) {
 			str = swap(str,s,e);
 			s++;
@@ -21,11 +18,27 @@ public class reverseString2 {
 		}
 		return str;
 	}
+	public static char[] reverseWordsInRow(char[] a) {
+		a = reverseString(a,0,a.length-1);
+		int rowLen = a.length - 1 ;
+		int start = 0;
+		int end = 0; 
+		while (a[end] != ' ') {
+			 if ( end != rowLen) {
+				 a = reverseString(a,start,end);
+				 break;
+			 }
+			 a = reverseString(a,start,end -1);
+			 start = end + 1 ;
+			 end = start;
+		}
+		return a;
+	}
 	public static void main(String args[]) {
 		Scanner scObj = new Scanner(System.in);
 		char string[] = scObj.nextLine().toCharArray();
-		System.out.println(reverseString(string));
-		
+		System.out.println(reverseString(string,0,string.length-1));
+		System.out.println(reverseWordsInRow(string));
 	}
 }
  
